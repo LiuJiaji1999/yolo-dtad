c649f33cd0b8c32d8215d43e35316bbb07adbd1d
LiuJiaji1999 <494882733@qq.com>
2025-02-26 20:40:24 +0800

    add

--- val.py
+++ val.py
@@ -3,13 +3,14 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('/home/lenovo/data/liujiaji/powerGit/yolov8/runs/8.1.9/exp112/weights/best.pt')
-    model.val(data='/home/lenovo/data/liujiaji/YOLO-DTAD/dataset/powerdata.yaml',
-              split='test',
+    model = YOLO('/home/lenovo/data/liujiaji/YOLO-DTAD/runs/train/exp/weights/bets.pt')
+    model.val(data='/home/lenovo/data/liujiaji/powerGit/dayolo/domain/sim10k_to_cityscapes.yaml',
+              split='val',
               imgsz=800, # 默认640
               batch=16,
               # rect=False,
               save_json=True, # if you need to cal coco metrice
-              project='runs/test',
-              name='exp112',
+              project='runs/val',
+              name='sourcesim10k',
               )
+# sim10k_to_cityscapes.yaml sourcesim10k
