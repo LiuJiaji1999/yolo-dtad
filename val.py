15c709516c67cd8eed3ea20e6c3bc7f845f60cab
LiuJiaji1999 <494882733@qq.com>
2025-11-05 16:54:29 +0800

    add

--- val.py
+++ val.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     model = YOLO('/home/lenovo/data/liujiaji/powerGit/yolov8/runs/8.1.9/exp119/weights/best.pt')
     model.val(data='/home/lenovo/data/liujiaji/YOLO-DTAD/dataset/VisDrone.yaml',
               split='test',
-              imgsz=800, # 默认640
+              imgsz=640, # 默认640
               batch=16,
               # rect=False,
               save_json=True, # if you need to cal coco metrice
