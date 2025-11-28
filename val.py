5de4cbb1413ff8cff061a7c77b7db04a8c5189c5
LiuJiaji1999 <494882733@qq.com>
2025-11-28 08:04:43 +0800

    add

--- val.py
+++ val.py
@@ -4,10 +4,10 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # model = YOLO('/home/lenovo/data/liujiaji/powerGit/yolov8/runs/8.1.9/exp119/weights/best.pt')
-    model = YOLO('/home/lenovo/data/liujiaji/YOLO-DTAD/runs/debug/mvexp3/weights/best.pt')
-    model.val(data='/home/lenovo/data/liujiaji/ultralytics-yolo11-main/dataset/powerdata.yaml',
+    model = YOLO('/home/lenovo/data/liujiaji/YOLO-DTAD/runs/debug/mvexp4/weights/best.pt')
+    model.val(data='/home/lenovo/data/liujiaji/ultralytics-yolo11-main/dataset/VisDrone.yaml',
               split='test',
-              imgsz=640, # 默认640
+              imgsz=800, # 默认640
               batch=2,
               # rect=False,
               save_json=True, # if you need to cal coco metrice
